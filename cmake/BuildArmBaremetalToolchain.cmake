87f303ec61851a0c6e4ffddd7646e6ba024508be
Logan Buchy <logan.buchy@gmail.com>
2014-11-16 18:05:12 -0800

    Refinement of build system * Should now build a basic program TODO: * Add the STM32 HAL drivers

--- cmake/BuildArmBaremetalToolchain.cmake
+++ cmake/BuildArmBaremetalToolchain.cmake
@@ -11,9 +11,9 @@ set( PKG_ARCHIVE_PREFIX "/gcc-arm-none-eabi-4_8-2014q3" )
 set( TOOLCHAIN_DIR "${CMAKE_CURRENT_BINARY_DIR}${PKG_EXTRACT_PREFIX}${PKG_ARCHIVE_PREFIX}" )
 
 set( CMAKE_SYSTEM_NAME "Generic" )
-set( CMAKE_C_COMPILER "${TOOLCHIAN_DIR}/bin/arm-nene-eabi-gcc" )
+set( CMAKE_C_COMPILER "${TOOLCHAIN_DIR}/bin/arm-none-eabi-gcc" )
 set( CMAKE_SHARED_LIBRARY_LINK_C_FLAGS )
 
 set( CMAKE_C_FLAGS
-     "--specs=nosys.specs -ffunction-sections -Wall -std=gnu99 -mflat-abi=soft -mthumb -g3 -mcpu=cortex-m4" )
+	"-mthumb -mcpu=cortex-m4 --specs=nosys.specs -ffunction-sections -Wall -std=gnu99 -mfloat-abi=soft -g3" )
 
--- cmake/BuildStm32CubeF4.cmake
+++ cmake/BuildStm32CubeF4.cmake
@@ -2,7 +2,22 @@
 set( PKG_URL "file://${CMAKE_SOURCE_DIR}/stm32cubef4.zip" )
 set( PKG_MD5 "52869aa3341045fbbe21cb1296a0a9d4" )
 
-set( PKG_ARCHIVE_PREFIX "STM32Cube_FW_F4_V1.3.0" )
-set( PKG_EXTRACT_PREFIX "STM32Cube" )
+set( PKG_ARCHIVE_PREFIX "/STM32Cube_FW_F4_V1.3.0" )
+set( PKG_EXTRACT_PREFIX "/STM32Cube" )
 
 set( PKG_CMAKELISTS "" )
+
+set( STM32CUBE_ROOT ${CMAKE_BINARY_DIR}${PKG_ARCHIVE_PREFIX}${PKG_EXTRACT_PREFIX} )
+
+aux_source_directory(
+        "${STM32CUBE_ROOT}/STM32F4xx_HAL_Driver/Src"
+	STM32CUBE_HAL_SRC)
+
+set( STM32CUBE_SRC ${STM32CUBE_SRC} 
+	"${STM32CUBE_ROOT}/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c"
+	"${STM32CUBE_ROOT}/Drivers/CMSIS/Device/ST/STM32F4xx/Source/Templates/gcc/startup_stm32f401xc.s"
+	${STM32CUBE_HAL_SRC} )
+
+include_directories( "${STM32CUBE_ROOT}/Drivers/CMSIS/Device/ST/STM32F4xx/Include" )
+include_directories( "${STM32CUBE_ROOT}/Drivers/CMSIS/Include" )
+include_directories( "${STM32CUBE_ROOT}/STM32F4xx_HAL_Driver/Inc" )
